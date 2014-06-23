c7bcf8f489ee990b014a7c87e7a968180e925b2a
yoogx <hugues.jerome@gmail.com>
2014-06-23 11:41:25 +0200

    * Complete this chapter

--- chap4.adb
+++ chap4.adb
@@ -20,4 +20,24 @@ package body Chap4 is
       return Result;
    end Max_Element;
 
+   -----------------
+   -- Min_Element --
+   -----------------
+
+   function Min_Element (A : T_Arr) return Positive is
+      Result : Positive := A'First;
+   begin
+      for J in A'First .. A'Last loop
+         if A (Result) > A (J) then
+            Result := J;
+         end if;
+         pragma Loop_Invariant
+           (Result in A'Range and
+            (for all K in A'First .. J => A (K) >= A (Result)) and
+            (for all K in A'First .. Result - 1 => A (K) > A (Result)));
+      end loop;
+
+      return Result;
+   end Min_Element;
+
 end Chap4;
--- chap4.ads
+++ chap4.ads
@@ -2,6 +2,10 @@
 --  "ACSL by Example" technical report, chapter 4, from
 --  http://www.fokus.fraunhofer.de/de/sqc/_download_sqc/ACSL-by-Example.pdf
 
+--  Note:
+--  * all examples from this chapter are implemented
+--  * GNATProve GPL2014 discharges all VCs of this package
+
 generic
    type T is (<>);
 
@@ -27,7 +31,10 @@ package Chap4 is
        (for all J in A'First .. Max_Element'Result - 1 =>
           A (J) < A (Max_Element'Result)));
 
-   --  4.4 The 'max_seq' algorithm
+      --  4.4 The 'max_seq' algorithm
+      --
+      --   max_seq returns the maximum value (not just the index of
+      --  it). Therefore, it requires a non- empty range as an argument.
 
    function Max_Seq (A : T_Arr) return T is (A (Max_Element (A))) with
       Pre  => (A'Length > 0),
@@ -35,8 +42,22 @@ package Chap4 is
       ((for all J in A'Range => Max_Seq'Result >= A (J)) and
        (for some J in A'Range => Max_Seq'Result = A (J)));
 
-   --  4.5 The 'min_element' algorithm
-   --
-   --  Note: equivalent to 4.3, left as an exercise
+      --  4.5 The 'min_element' algorithm
+      --
+   --  The function finds the largest element in the range a[0, n).
+   --  More precisely, it returns the unique valid index i such that
+   --  1. for each index k with 0 <= k < n the condition a[k] >= a[i] holds and
+   --  2. for each index k with 0 <= k < i the condition a[k] > a[i] holds.
+      --
+      --  The return value of max_element is n if and only if there is no
+      --  maximum, which can only occur if n == 0.
+
+   function Min_Element (A : T_Arr) return Positive with
+      Pre  => (A'Length > 0),
+      Post =>
+      ((Min_Element'Result in A'Range) and
+       (for all J in A'Range => A (J) >= A (Min_Element'Result)) and
+       (for all J in A'First .. Min_Element'Result - 1 =>
+          A (J) > A (Min_Element'Result)));
 
 end Chap4;
