a6154d9d749f60054e1d9840ebfdde41b0958f34
Christophe Garion <tofgarion@runbox.com>
2016-06-29 17:07:39 +0200

    chap3: add postcondition to Find + put helper functions before functions

--- chap3.ads
+++ chap3.ads
@@ -11,14 +11,8 @@ package Chap3 with
 
    --  3.1 The 'equal' algorithm
    --
-   --  The function returns true if a[i] == b[i] holds for each 0 <= i < n.
-   --  Otherwise, equal returns false.
+   --  Is_Equal is an helper function to write clearer assertions.
 
-   function Equal (A : T_Arr; B : T_Arr; Size : Natural) return Boolean with
-     Pre  => (Size <= A'Length and Size <= B'Length),
-     Post => (Equal'Result = Is_Equal (A, B, Size));
-
-   -- Is_Equal is an helper function to write clearer assertions.
    function Is_Equal
      (A    : T_Arr;
       B    : T_Arr;
@@ -26,9 +20,16 @@ package Chap3 with
      (for all I in 0 .. Size - 1 => A (A'First + I) = B (B'First + I)) with
        Pre => (Size <= A'Length and Size <= B'Length);
 
+   --  The function Equal returns true if a[i] == b[i] holds for each
+   --  0 <= i < n.  Otherwise, equal returns false.
+
+   function Equal (A : T_Arr; B : T_Arr; Size : Natural) return Boolean with
+     Pre  => (Size <= A'Length and Size <= B'Length),
+     Post => (Equal'Result = Is_Equal (A, B, Size));
+
    --  3.2 The 'mismatch' algorithm
    --
-   --  The function mismatch returns the smallest index where the two
+   --  The function Mismatch returns the smallest index where the two
    --  ranges a and b differ. If no such index exists, that is, if
    --  both ranges are equal then mismatch returns the length n of
    --  the two ranges.
@@ -37,40 +38,43 @@ package Chap3 with
      (A    : T_Arr;
       B    : T_Arr;
       Size : Positive) return Natural with
-        Pre  => (Size <= A'Length and Size <= B'Length),
-        Post => (Mismatch'Result <= Size and then
-                 Is_Equal (A, B, Mismatch'Result)),
-        Contract_Cases =>
-          (Is_Equal (A, B, Size) => Mismatch'Result = Size,
-           others                => Mismatch'Result < Size and then
-             A (A'First + Mismatch'Result) /= B (B'First + Mismatch'Result));
+     Pre  => (Size <= A'Length and Size <= B'Length),
+     Post => (Mismatch'Result <= Size and then
+              Is_Equal (A, B, Mismatch'Result)),
+     Contract_Cases =>
+       (Is_Equal (A, B, Size) => Mismatch'Result = Size,
+        others                =>
+          Mismatch'Result < Size and then
+          A (A'First + Mismatch'Result) /= B (B'First + Mismatch'Result)
+       );
 
    --  3.3 The 'find' algorithm
    --
-   --  The function find returns the least valid index i of a where
+   --  Has_Value is an helper function to write clearer assertions.
+   function Has_Value
+     (A    : T_Arr;
+      Size : Natural;
+      Val  : T) return Boolean is
+     (for some I in 0 .. Size - 1 => A (A'First + I) = Val) with
+     Pre => Size <= A'Length;
+
+   --  The function Find returns the least valid index i of A where
    --  the condition a[i] == val holds.  If no such index exists then
    --  find returns the length n of the array.
 
    function Find (A : T_Arr; Size : Positive; Val : T) return Natural with
      Pre  => (Size <= A'Length),
      Post => (Find'Result <= Size and then
-              not Has_Value(A, Find'Result, Val)),
+                not Has_Value(A, Find'Result, Val)),
      Contract_Cases =>
        (Has_Value(A, Size, Val) =>
-           Find'Result < Size and then
-           A (A'First + Find'Result) = Val,
+          Find'Result < Size and then
+          A (A'First + Find'Result) = Val and then
+          not Has_Value(A, Find'Result, Val),
         others                  => Find'Result = Size
        );
 
-   -- Has_Value is an helper function to write clearer assertions.
-   function Has_Value
-   (A    : T_Arr;
-    Size : Natural;
-    Val  : T) return Boolean is
-    (for some I in 0 .. Size - 1 => A (A'First + I) = Val) with
-      Pre => Size <= A'Length;
-
-   --  3.5 The find_first_of Algorithm
+   --  3.5 The 'find_first_of' Algorithm
    --
    -- As in Find this function performs a sequential search. However,
    -- whereas find searches for a particular value, find_first_of
