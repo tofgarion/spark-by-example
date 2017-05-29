31d44d2591d295681f14fd81174d1fb91b377dda
Christophe Garion <tofgarion@runbox.com>
2017-05-29 10:29:51 +0200

    non-mutating: add example for completeness/disjointness

---
+++ non-mutating/naive_find_contract_pb.ads
@@ -0,0 +1,17 @@
+-- Naive Find function specification
+
+with Types; use Types;
+
+package Naive_Find_Contract_Pb with
+     Spark_Mode is
+
+   function Find (A : T_Arr; Val : T) return Positive with
+      Post =>
+      (Find'Result <= A'Last + 1
+       and then (for all I in A'First .. Find'Result - 1 => A (I) /= Val)),
+      Contract_Cases =>
+      ((for some I in A'Range => A (I) = Val) =>
+         Find'Result <= A'Last and then A (Find'Result) = Val,
+       (for some I in A'Range => A (I) /= Val) => Find'Result = A'Last + 1);
+
+end Naive_Find_Contract_Pb;
