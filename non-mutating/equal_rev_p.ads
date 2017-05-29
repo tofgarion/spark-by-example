af8e998268bf6f918c650c6a4e2c775c0b6ffdb3
Christophe Garion <tofgarion@runbox.com>
2017-05-29 17:42:00 +0200

    non-mutating: add a "reverse" implementation of Equal

---
+++ non-mutating/equal_rev_p.ads
@@ -0,0 +1,16 @@
+-- specification of Equal ("reversed" version)
+
+with Types;          use Types;
+with Equal_Ranges_P; use Equal_Ranges_P;
+with Mismatch_P;     use Mismatch_P;
+
+package Equal_Rev_P with
+     Spark_Mode is
+
+   function Equal
+     (A : T_Arr;
+      B : T_Arr) return Boolean is
+     (A = B (B'First .. B'First - 1 + A'Length)) with
+      Pre  => A'Length <= B'Length,
+      Post => Equal'Result = (not Mismatch (A, B).Exists);
+end Equal_Rev_P;
