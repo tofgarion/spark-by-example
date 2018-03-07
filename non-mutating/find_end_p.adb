3ed1a2e9d535252157dd2a9aa2ec26b0f05c4fcc
Léo Creuse <leo.creuse@gmail.com>
2018-03-07 12:08:24 +0100

    trying some tests, compiler states "is" missing at begining of every packege while located after Spark_Mode.

--- non-mutating/find_end_p.adb
+++ non-mutating/find_end_p.adb
@@ -1,2 +1,34 @@
 --implemenation if Find_end
 
+package body Find_End_P with
+  Spark_Mode is
+   
+   function Find_End (A : T_Arr; B : T_Arr) return Option is
+      Result :Option :=(Exists => False);
+   begin
+      if(A'Length<B'Length or else B'Length = 0) then
+	 return Result;
+      end if;
+      
+      for I in A'First .. A'Last +1 - B'Length loop
+	 if A(I..I-1+B'Length) = B then
+	    Result := (Exists => True, Value => I);
+	    
+	 end if;
+	 
+	 
+	 pragma Loop_Variant (Increases => I);
+	 pragma Loop_Invariant(if Result.Exists
+				 then Has_Sub_Range_In_Postfix(A,I,B)
+	                         and then not Has_Sub_Range(A(Result.Value +1 .. I+B'Length-1),B)
+	                         else not Has_Sub_Range_In_Prefix(A,I,B));
+	 
+	 
+      end loop;
+      
+      
+      return Result;
+      
+   end Find_End;
+end Find_End_P;
+
--- non-mutating/find_end_p.ads
+++ non-mutating/find_end_p.ads
@@ -4,12 +4,11 @@ with Types;           use Types;
 with Has_Sub_Range_P; use Has_Sub_Range_P;
 
 
-package Find_End_P with
-  Spark_Mode is
+package Find_End_P with SPARK_Mode is
    
    Function Find_End (A : T_Arr; B: T_arr) return Option with
      Pre            =>A'Last < Positive'Last and then B'First <= B'Last,
-     Contract Cases =>
+     Contract_Cases =>
      (B'Length = 0 => not Find_End'Result.Exists,
       A'Length < B'Length => not Find_End'Result.Exists,
       A'Length >=B'Length and then Has_Sub_Range(A,B) =>
@@ -19,8 +18,8 @@ package Find_End_P with
 	(if
 	   Find_End'Result.Value < A'Last+1-B'Length
 	   then
-             (not Has_Sub_Range_In_Postfix(A, Find_End'Result.Value-1,B))),
-       others  => not Search'Result.Exists;
+             (not Has_Sub_Range_In_Postfix(A, Find_End'Result.Value,B))),
+       others  => not Find_End'Result.Exists);
      
      
      
