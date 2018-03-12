06f9710e728ce4f0c95703c0f92e90e69cfd166e
Léo Creuse <leo.creuse@gmail.com>
2018-03-12 14:53:49 +0100

    added back forgotten file

---
+++ spec/lower_bound_p.ads
@@ -0,0 +1,11 @@
+package Lower_Bound_P with
+  SPARK_Mode,
+  Ghost is
+   
+   function Lower_Bound(A: T_Arr; N: Positive; V: T) return Boolean is
+      (for all I in A'First .. N => A(I)>=V);
+      
+   function Strict_Lower_Bound(A(T_Arr; N: Positive; V: T) Returb Boolean is
+      (for all I in A'First .. N => A(I)>V);
+      
+end Lower_Bound_P;
