9d0eea1a31c097733c450ac06a82aadfd82852c2
Léo Creuse <leo.creuse@gmail.com>
2018-03-13 11:04:22 +0100

    finished working on Search_Lower_Bound

--- binary-search/search_lower_bound_p.ads
+++ binary-search/search_lower_bound_p.ads
@@ -1,6 +1,7 @@
 with Types; use Types;
 with Sorted_P; use Sorted_P;
 with Lower_Bound_P; use Lower_Bound_P;
+with Upper_Bound_P; use Upper_Bound_P;
 
 package Search_Lower_Bound_P with
   SPARK_Mode is
@@ -8,13 +9,14 @@ package Search_Lower_Bound_P with
    function Search_Lower_Bound
      (A: T_Arr;
 	V: T) return Option with
-     Pre => Sorted(A),
+     Pre => Sorted(A) and then
+     A'Last < Positive'Last,
      Contract_Cases => 
-     (A'Length = 0 => not Result.Exists,
-      A'Length > 0 => Result.Exists
-	and then A'First <= Result.Value
-	and then Result.Value <= A'Last+1
-	and then Strict_Lower_Bound(A(A'First .. Search_Lower_Bound'Result.Value-1),V)
+     (A'Length = 0 => not Search_Lower_Bound'Result.Exists,
+      A'Length > 0 => Search_Lower_Bound'Result.Exists
+	and then A'First <= Search_Lower_Bound'Result.Value
+	and then Search_Lower_Bound'Result.Value <= A'Last+1
+	and then Strict_Upper_Bound(A(A'First .. Search_Lower_Bound'Result.Value-1),V)
 	and then Lower_Bound(A(Search_Lower_Bound'Result.Value .. A'Last), V));
      
-end Search_Lower_Bound;
+end Search_Lower_Bound_P;
