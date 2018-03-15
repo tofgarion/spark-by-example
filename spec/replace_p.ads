5cc7b9439287f4e21d995a75db8065d42f7130b0
Joffrey Huguet <huguet.joffrey@gmail.com>
2018-03-15 09:40:59 +0100

    6-mutating : added replace predicate

--- spec/replace_p.ads
+++ spec/replace_p.ads
@@ -6,4 +6,5 @@ package Replace_P with
    function Replace ( A : T_Arr ; B : T_Arr ; Val : T ; Rep : T ) return Boolean is
       (for all I in A'Range => if A(I) = Val then B(I) = Rep else B(I)=A(I)) with
 	Pre => A'Length = B'Length;
-      end Replace_P;
+      
+end Replace_P;
