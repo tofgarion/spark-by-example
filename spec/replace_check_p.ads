7436485143ecd7fc96a46d18dd9fa766654fee17
Joffrey Huguet <huguet.joffrey@gmail.com>
2018-03-15 09:48:12 +0100

    6-mutating: renamed replace_p.ads predicate to replace_check_p.ads

--- spec/replace_check_p.ads
+++ spec/replace_check_p.ads
@@ -1,10 +1,10 @@
 with Types; use Types;
 
-package Replace_P with
+package Replace_Check_P with
   Spark_Mode,
   Ghost is
-   function Replace ( A : T_Arr ; B : T_Arr ; Val : T ; Rep : T ) return Boolean is
+   function Replace_Check ( A : T_Arr ; B : T_Arr ; Val : T ; Rep : T ) return Boolean is
       (for all I in A'Range => if A(I) = Val then B(I) = Rep else B(I)=A(I)) with
 	Pre => A'Length = B'Length;
       
-end Replace_P;
+end Replace_Check_P;
