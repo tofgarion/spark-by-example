c786d23a8f9175de1221a853a247a15e18aa3aee
Joffrey Huguet <huguet.joffrey@gmail.com>
2018-03-20 09:24:57 +0100

    remove_copy: added assertions but can't prove with timeout 600

--- mutating/remove_copy_p.adb
+++ mutating/remove_copy_p.adb
@@ -1,40 +1,32 @@
 package body Remove_Copy_P with
      Spark_Mode is
-   procedure Remove_Copy (A : T_Arr; B : in out T_Arr; Val : T) is
+   procedure Remove_Copy (A : in T_Arr; B : in out T_Arr; Val : T) is
       J : Integer := B'First;
    begin
       for I in A'Range loop
          if A (I) /= Val then
-	    
+	    pragma Assert(Multiset_Retain_Rest(A (A'First .. I - 1), B( B'First .. J-1), Val));
             B (J) := A (I);
-	    pragma Assert(if J > B'First then Multiset_Retain_Rest(A(A'First .. I-1), B(B'First .. J-1), Val));
-	    pragma Assert(if J > B'First then Multiset_Retain_Rest(A(A'First .. I), B(B'First .. J), Val));
-            J     := J + 1;
-	    
+	    J     := J + 1;
+	    pragma Assert(Count (A(A'First .. I), A(I)) = Count (B(B'First .. J-1), A(I)));
+	    pragma Assert(Multiset_Retain_Rest(A (A'First .. I), B(B'First .. J-1), Val));
          end if;
 
-         pragma Loop_Invariant (B'First <= J);
-	 pragma Loop_Invariant (J - B'First <= I - A'First + 1);
-         pragma Loop_Invariant (J <= B'Last+1);-- or else (J = B'Last + 1 and then (for all K in I+1.. A'Last => A(K)=Val)));
+         pragma Loop_Invariant (J in B'First .. B'Last + 1);
          pragma Loop_Invariant
-           (J - B'First = I - A'First - Count (A (A'First .. I), Val) + 1);
-	 pragma Loop_Invariant (if J = B'First then (for all K in A'First .. I => A(K) = Val));
-	 --pragma Loop_Invariant (if (for all K in I+1.. A'Last => A(K)=Val) then J=B'Last +1);
+           (J = B'First + I - A'First - Count (A (A'First .. I), Val) + 1);
+         pragma Loop_Invariant
+           (if J = B'First then (for all K in A'First .. I => A (K) = Val));
          pragma Loop_Invariant
            (if
               J > B'First
             then
-	   Multiset_Retain_Rest( A(A'First .. I), B(B'First .. J-1), Val)
-	   );
-	
-	  
-	 pragma Loop_Invariant (for all K in B'First .. J-1 => B(K) /= Val);
-	 --pragma Loop_Invariant (if J = B'Last+1 then (for all  K in I+1 .. A'Last => A(K) = Val));
+              Multiset_Retain_Rest
+                (A (A'First .. I),
+                 B (B'First .. J - 1),
+                 Val));
+         pragma Loop_Invariant (for all K in B'First .. J - 1 => B (K) /= Val);
+
 	   end loop;
-
---pragma Assert (J= B'Last + 1);
---pragma Assert (Multiset_Retain_Rest(A(A'First .. A'Last), B(B'First .. J-1),Val));
---pragma Assert  (Multiset_Retain_Rest (A, B, Val));
-
-   end Remove_Copy;
+end Remove_Copy;
 end Remove_Copy_P;
--- mutating/remove_copy_p.ads
+++ mutating/remove_copy_p.ads
@@ -1,12 +1,13 @@
-with Types;                  use Types;
-with Multiset_Retain_Rest_Bis_P; use Multiset_Retain_Rest_Bis_P;
-with Has_Value_P;            use Has_Value_P;
-with Count_P;                use Count_P;
+with Types;                      use Types;
+with Multiset_Retain_Rest_P; use Multiset_Retain_Rest_P;
+with Has_Value_P;                use Has_Value_P;
+with Count_P;                    use Count_P;
 
 package Remove_Copy_P with
      Spark_Mode is
-   procedure Remove_Copy (A : T_Arr; B : in out T_Arr; Val : T) with
+   procedure Remove_Copy (A : in T_Arr; B : in out T_Arr; Val : T) with
       Pre => B'Last = B'First + A'Length - Count (A, Val) - 1
       and then B'Last < Positive'Last,
-      Post => Multiset_Retain_Rest (A, B, Val) and then (for all I in B'Range => B(I) /= Val);
+      Post => Multiset_Retain_Rest (A, B, Val)
+      and then (for all I in B'Range => B (I) /= Val);
 end Remove_Copy_P;
