7b3214ce1dff22e51326901a1750e647b6500c03
Joffrey Huguet <huguet.joffrey@gmail.com>
2018-03-21 10:11:25 +0100

    iota: function is proved, Iota.org written

---
+++ numeric/iota_p.adb
@@ -0,0 +1,13 @@
+package body Iota_P with
+     Spark_Mode is
+   procedure Iota (A : in out T_Arr; Val : T) is
+      Count : T := Val;
+   begin
+      for I in A'Range loop
+         A (I) := Count;
+         Count := Count + 1;
+         pragma Loop_Invariant (Count = Val + T (I - A'First + 1));
+         pragma Loop_Invariant (Is_Iota (A (A'First .. I), Val));
+      end loop;
+   end Iota;
+end Iota_P;
