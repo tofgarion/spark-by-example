864efd9c17677921a88de3e5151a98d034ba47f9
Christophe Garion <tofgarion@runbox.com>
2018-03-21 13:45:19 +0100

    non-mutating: review of Find_End + add corresponding targets in Makefile

--- non-mutating/find_end_p.adb
+++ non-mutating/find_end_p.adb
@@ -1,38 +1,41 @@
 --implemenation if Find_end
 
 package body Find_End_P with
-  Spark_Mode is
-   
+     Spark_Mode is
+
    function Find_End (A : T_Arr; B : T_Arr) return Option is
-      Result :Option :=(Exists => False);
+      Result : Option := (Exists => False);
    begin
-      if(A'Length<B'Length or else B'Length = 0) then
-	 return Result;
+      if (A'Length < B'Length or else B'Length = 0) then
+         return Result;
       end if;
-      
-      for I in A'First .. A'Last - B'Length +1 loop
-	 if A(I..I-1+B'Length) = B then
-	    Result := (Exists => True, Value => I);
-	    
-	 end if;
-	 
-	 
-	 pragma Loop_Variant (Increases => I);
-	 pragma Loop_Invariant(if Result.Exists
-				 then Equal_Subrange(A,Result.Value,B)
-	                         and then Result.Value in A'First .. A'Last-B'Length +1
-	                         and then Result.Value <=I
-	                         and then (if Result.Value<I
-					     then (for all J in Result.Value+1 .. I => not Equal_Subrange(A,J,B))
-	                                     else True)
-	                         else not Has_Sub_Range_In_Prefix(A,I,B));
-	 
-	 
+
+      for I in A'First .. A'Last - B'Length + 1 loop
+         if A (I .. I - 1 + B'Length) = B then
+            Result := (Exists => True, Value => I);
+
+         end if;
+
+         pragma Loop_Variant (Increases => I);
+         pragma Loop_Invariant
+           (if
+              Result.Exists
+            then
+              Equal_Subrange (A, Result.Value, B)
+              and then Result.Value in A'First .. A'Last - B'Length + 1
+              and then Result.Value <= I
+              and then
+              (if
+                 Result.Value < I
+               then
+                 (for all J in Result.Value + 1 .. I =>
+                    not Equal_Subrange (A, J, B))
+               else True)
+            else not Has_Sub_Range_In_Prefix (A, I, B));
+
       end loop;
-      
-      
+
       return Result;
-      
+
    end Find_End;
 end Find_End_P;
-
--- non-mutating/find_end_p.ads
+++ non-mutating/find_end_p.ads
@@ -3,25 +3,22 @@
 with Types;           use Types;
 with Has_Sub_Range_P; use Has_Sub_Range_P;
 
+package Find_End_P with
+     Spark_Mode is
 
-package Find_End_P with SPARK_Mode is
-   
-   Function Find_End (A : T_Arr; B: T_arr) return Option with
-     Pre            =>A'Last < Positive'Last and then B'First <= B'Last,
-     Contract_Cases =>
-     (B'Length = 0 => not Find_End'Result.Exists,
-      A'Length < B'Length => not Find_End'Result.Exists,
-      A'Length >=B'Length and then Has_Sub_Range(A,B) =>
-	Find_End'Result.Exists
-	and then Equal_Subrange(A, Find_End'Result.Value , B)
-	and then 
-	(if
-	   Find_End'Result.Value < A'Last-B'Length+1
-	   then
-             (not Has_Sub_Range_In_Postfix(A, Find_End'Result.Value+1,B))),
-       others  => not Find_End'Result.Exists);
-     
-     
-     
+   function Find_End (A : T_Arr; B : T_Arr) return Option with
+      Pre            => A'Last < Positive'Last and then B'First <= B'Last,
+      Contract_Cases =>
+      (B'Length = 0 => not Find_End'Result.Exists,
+       A'Length < B'Length => not Find_End'Result.Exists,
+       A'Length >= B'Length and then Has_Sub_Range (A, B) =>
+         Find_End'Result.Exists
+         and then Equal_Subrange (A, Find_End'Result.Value, B)
+         and then
+         (if
+            Find_End'Result.Value < A'Last - B'Length + 1
+          then
+            (not Has_Sub_Range_In_Postfix (A, Find_End'Result.Value + 1, B))),
+       others => not Find_End'Result.Exists);
+
 end Find_End_P;
-   
