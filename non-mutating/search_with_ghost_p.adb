a7e6aec6ca4a309c6560f2d8109848d873845408
Christophe Garion <tofgarion@runbox.com>
2018-03-21 13:07:48 +0100

    non-mutating: review of Search + add corresponding targets in Makefile (still pb)

--- non-mutating/search_with_ghost_p.adb
+++ non-mutating/search_with_ghost_p.adb
@@ -1,6 +1,6 @@
 -- implementation of Search
 
-package body Search_With_Ghost with
+package body Search_With_Ghost_P with
      Spark_Mode is
 
    function Search (A : T_Arr; B : T_Arr) return Option is
@@ -27,4 +27,4 @@ package body Search_With_Ghost with
       return Result;
    end Search;
 
-end Search_With_Ghost;
+end Search_With_Ghost_P;
--- non-mutating/search_with_ghost_p.ads
+++ non-mutating/search_with_ghost_p.ads
@@ -1,17 +1,23 @@
 with Types; use Types;
 
-package Search_WO_Ghost with
+package Search_With_Ghost_P with
      Spark_Mode is
+
+   function Has_Sub_Range
+     (A : T_Arr;
+      B : T_Arr) return Boolean is
+     (for some J in A'First .. A'Last + 1 - B'Length =>
+        A (J .. J - 1 + B'Length) = B) with
+      Pre => A'Length >= B'Length and then A'Last < Positive'Last,
+      Ghost;
 
    function Search (A : T_Arr; B : T_Arr) return Option with
       Pre            => A'Last < Positive'Last and then B'First <= B'Last,
       Contract_Cases =>
       (B'Length = 0        => not Search'Result.Exists,
        A'Length < B'Length => not Search'Result.Exists,
-       A'Length >= B'Length
-       and then
-       (for some J in A'First .. A'Last - B'Length + 1 =>
-          A (J .. J - 1 + B'Length) = B) =>
+       A'Length >= B'Length and then
+       Has_Sub_Range (A, B) =>
          Search'Result.Exists
          and then
            A (Search'Result.Value .. Search'Result.Value - 1 + B'Length) =
@@ -20,8 +26,9 @@ package Search_WO_Ghost with
          (if
             Search'Result.Value > A'First
           then
-            (for all K in A'First .. Search'Result.Value - 1 =>
-               A (K .. K - 1 + B'Length) /= B)),
+            (not Has_Sub_Range
+               (A (A'First .. Search'Result.Value + B'Length - 2),
+                B))),
        others => not Search'Result.Exists);
 
-end Search_WO_Ghost;
+end Search_With_Ghost_P;
