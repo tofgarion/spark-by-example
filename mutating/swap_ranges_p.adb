3757e3bec0b362fa9576f70aaa4caebfa881a085
Joffrey Huguet <huguet.joffrey@gmail.com>
2018-03-22 09:25:43 +0100

    swap_ranges: modified .adb file

--- mutating/swap_ranges_p.adb
+++ mutating/swap_ranges_p.adb
@@ -3,18 +3,18 @@ package body Swap_Ranges_P with
    procedure Swap_Ranges (A : in out T_Arr; B : in out T_Arr) is
    begin
 
-      for I in A'Range loop
-         Swap (A (I), B (B'First + (I- A'First)));
+       for J in 0 .. A'Length - 1 loop
+         Swap (A (A'First + J), B (B'First + J));
 
          pragma Loop_Invariant
-           (B'Loop_Entry (B'First .. B'First + (I - A'First))= A(A'First .. I));
+           (B'Loop_Entry (B'First .. B'First + J)= A(A'First .. A'First + J));
          pragma Loop_Invariant
-           (A'Loop_Entry (A'First .. I) =
-	      B (B'First .. B'First + (I - A'First)));
-	 pragma Loop_Invariant (if B'First + (I-A'First) < B'Last then
-	   B'Loop_Entry (B'First + (I-A'First) + 1 .. B'Last) = B (B'First + (I-A'First) + 1 .. B'Last));
-	 pragma Loop_Invariant (if I < A'Last then
-	   A'Loop_Entry (I+1 .. A'Last) = A(I+1 .. A'Last));
+           (A'Loop_Entry (A'First .. A'First + J) =
+	      B (B'First .. B'First + J));
+	 pragma Loop_Invariant (if B'First + J < B'Last then
+	   B'Loop_Entry (B'First +J + 1 .. B'Last) = B (B'First +J + 1 .. B'Last));
+	 pragma Loop_Invariant (if A'First + J < A'Last then
+	   A'Loop_Entry (A'First + J +1 .. A'Last) = A(A'First + J +1 .. A'Last));
 	 
       end loop;
    end Swap_Ranges;
