d06a93545a52eca5416a3d58c6436e03341e8c69
Joffrey Huguet <huguet.joffrey@gmail.com>
2018-03-26 16:35:01 +0200

    modified types.ads

--- spec/types.ads
+++ spec/types.ads
@@ -2,9 +2,8 @@
 
 package Types is
    type T is new Integer;
-   type T_Arr is array (Positive range <>) of T;
-   type H_Arr is array (Natural range <>) of T;
-   type MAX_SIZE is constant Positive := 1000;
+   type T_Arr is array (Natural range <>) of T;
+   MAX_SIZE : constant Positive := 1000;
 
    type Option (Exists : Boolean := False) is record
       case Exists is
@@ -26,7 +25,7 @@ package Types is
    end record;
    
    type Heap  is record
-      A : H_Arr(0 .. MAX_SIZE);
+      A : T_Arr(0 .. MAX_SIZE);
       Size : Natural := 0;
    end record;
    
