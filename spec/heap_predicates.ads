b3b2fa4ff5f287d7d5323ea68379c6bcdd9d1245
Léo Creuse <leo.creuse@gmail.com>
2018-03-26 14:32:35 +0200

    added missing predicate file

--- spec/heap_predicates.ads
+++ spec/heap_predicates.ads
@@ -11,7 +11,7 @@ package Heap_Predicates with
    function Heap_Parent(I : Natural) return Natural is ((I-1)/2);
    
    function Is_Heap_Def(H : Heap) return Boolean is
-      (H.Size = 0 or else (for all I in 1 .. H.Size => H.A(I) <= H.A(Heap_Parent(I))));
+      (H.Size = 0 or else (for all I in 1 .. H.Size-1 => H.A(I) <= H.A(Heap_Parent(I))));
       
 end Heap_Predicates;
 
