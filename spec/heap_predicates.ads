ab99d8d52fb75b1bbdba7531f1f556a4ac9d7aff
Léo Creuse <leo.creuse@gmail.com>
2018-03-26 13:41:00 +0200

    Heap : wrote some heap_predicates

---
+++ spec/heap_predicates.ads
@@ -0,0 +1,18 @@
+with Types; use Types;
+
+package Heap_Predicates with
+  SPARK_Mode,
+  Ghost is
+   
+   function Heap_Left(I : Natural) return Natural is (2*I+1);
+   
+   function Heap_Right(I : Natural) return Natural is (2*I+2);
+   
+   function Heap_Parent(I : Natural) return Natural is ((I-1)/2);
+   
+   function Is_Heap(H : Heap) return Boolean is
+      (H.Size = 0 or else (for all I in 1 .. H.Size => H.A(I) <= H.A(Heap_Parent(I))));
+      
+end Heap_Predicates;
+
+      
