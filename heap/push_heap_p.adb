9384d98d7fa282e15d16c2bd6adc18d75cbf1d64
Joffrey Huguet <huguet.joffrey@gmail.com>
2018-03-27 10:40:06 +0200

    finished push heap function, need to prove

--- heap/push_heap_p.adb
+++ heap/push_heap_p.adb
@@ -1,7 +1,7 @@
 package body Push_Heap_P with
      Spark_Mode is
    procedure Push_Heap (H : in out Heap) is
-      V      : T := H.A (H.Size - 1);
+      V      : T := H.A (H.Size);
       Hole   : Natural;
       Parent : Natural;
       Old_V  : T with
@@ -10,15 +10,11 @@ package body Push_Heap_P with
          Ghost;
    begin
       if 1 < H.Size then
-         Hole := Heap_Parent (H.Size - 1);
+         Hole := Heap_Parent (H.Size);
          if H.A (Hole) < V then
 
-            H.A (H.Size - 1) := H.A (Hole);
-          --  pragma Assert (Is_Heap (H));
-          --  pragma Assert (Multiset_Add (H.A, Old_A, H.A (Hole)));
-          --  pragma Assert (Multiset_Minus (H.A, Old_A, V));
-          --  pragma Assert
-           --   (Multiset_Retain_Rest_Double (H.A, Old_A, V, H.A (Hole)));
+            H.A (H.Size) := H.A (Hole);
+	    
             if 0 < Hole then
                Parent := Heap_Parent (Hole);
                while Hole > 0 and then H.A (Parent) < V loop
@@ -31,7 +27,7 @@ package body Push_Heap_P with
                      Parent := Heap_Parent (Hole);
                   end if;
 		  
-	          pragma Loop_Invariant(Hole in 0 .. H. Size-1);
+	          pragma Loop_Invariant(Hole in 1 .. H. Size);
 		  pragma Loop_Invariant(Is_Heap(H));
 		  pragma Loop_Invariant(Parent = Heap_Parent(Hole));
 		  pragma Loop_Invariant(H.A(Hole) < V);
