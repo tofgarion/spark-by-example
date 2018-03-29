05f00a4be66d82b822e56dc20f75d5fb32f85f67
Léo Creuse <leo.creuse@gmail.com>
2018-03-29 09:29:24 +0200

    added forgotten file

--- mutating/swap_array_p.adb
+++ mutating/swap_array_p.adb
@@ -2,12 +2,41 @@ package body Swap_Array_P with
   SPARK_Mode is
    
     function Swap_Array(A : T_Arr; I : Positive; J : Positive)return T_Arr is
-      Result : T_Arr :=A;
+      Result : T_Arr (A'Range) :=A;
       Temp : T := Result(I);
+      
+      --ghost variables
+      
+      Interm : T_Arr (A'Range) with Ghost;
+      
+      -- ghost procedure
+      
+      procedure Prove_Perm with Ghost,
+        Pre => I in A'Range and then J in A'Range
+        and then Is_Set(A,I,A(J),Interm)
+        and then Is_Set(Interm,J,A(I),Result),
+        Post => Multiset_Unchanged(A,Result)
+      is
+      begin
+         for V in T loop
+            Occ_Set(A,Interm,I,A(J),V);
+            Occ_Set(Interm,Result,J,A(I),V);
+            pragma Loop_Invariant
+              (for all F in T'First .. V =>
+                 Occ(Result,F) = Occ(A,F));
+         end loop;
+      end Prove_Perm;
+      
    begin
       Result(I) := Result(J);
+      Interm := Result;
+      
+      pragma Assert(Is_Set(A,I,A(J),Result));
+
       Result(J) := Temp;
       
+      pragma Assert(Is_Set(Interm,J,A(I),Result));
+      Prove_Perm;
       return Result;
    end Swap_Array;
 end Swap_Array_P;
