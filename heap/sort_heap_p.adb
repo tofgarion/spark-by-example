ef217831fafa9e4f72d6fb8310b66353fa5112a4
Léo Creuse <leo.creuse@gmail.com>
2018-04-11 16:23:02 +0200

    proof of sort_heap finished, some modifications were added to pop_heap

--- heap/sort_heap_p.adb
+++ heap/sort_heap_p.adb
@@ -5,14 +5,25 @@ package body Sort_Heap_P with
    procedure Sort_Heap(H : in out Heap)
    is
       Interm : Heap := H with ghost;
+      N : constant Integer := H.Size;
    begin
-      while H.Size > 0 loop
+      Interm := H;
+      pragma assert(Interm.Size <= N);
+      For J in  reverse 1 .. N loop
          Interm := H;
+         --pragma assert(if J /= N then H.A(J +1)>= H.A(1));
+
          Pop_Heap(H);
+
+
+         pragma assert(H.Size+1 <= N);
+         pragma Loop_Invariant(H.A(J) >= H.A(1));
+         pragma Loop_Invariant(H.Size = J-1);
          pragma Loop_Invariant(Is_Heap_Def(H));
          pragma Loop_Invariant(Multiset_Unchanged(H.A,H'Loop_Entry.A));
+         pragma Loop_Invariant(Lower_Bound(H.A(J .. N), H.A(J)));
+         pragma Loop_Invariant(Sorted(H.A(J .. N)));
       end loop;
-
    end Sort_Heap;
 
 end Sort_Heap_P;
--- spec/multiset_predicates.ads
+++ spec/multiset_predicates.ads
@@ -1,5 +1,5 @@
 with Types;   use Types;
-with Count_P; use Count_P;
+with Occ_P; use Occ_P;
 
 package Multiset_Predicates with
      Spark_Mode,
@@ -9,34 +9,33 @@ package Multiset_Predicates with
      (A   : T_Arr;
       B   : T_Arr;
       Val : T) return Boolean is
-     (for all X in T => (if X /= Val then Count (A, X) = Count (B, X)));
+     (for all X in T => (if X /= Val then Occ (A, X) = Occ (B, X)));
 
-   function Multiset_Retain_Rest
+   function Multiset_Retain_Rest_Double
      (A    : T_Arr;
       B    : T_Arr;
       Val1 : T;
       Val2 : T) return Boolean is
      (for all X in T =>
-        (if X /= Val1 and then X /= Val2 then Count (A, X) = Count (B, X)));
+        (if X /= Val1 and then X /= Val2 then Occ (A, X) = Occ (B, X)));
 
    function Multiset_Unchanged
      (A : T_Arr;
       B : T_Arr) return Boolean is
-     (for all K in T => Count (A, K) = Count (B, K)) with
+     (for all K in T => Occ (A, K) = Occ (B, K)) with
       Pre => A'Length = B'Length;
 
    function Multiset_Add
      (A   : T_Arr;
       B   : T_Arr;
       Val : T) return Boolean is
-     (Count (A, Val) = Count (B, Val) + 1) with
-      Pre => A'Length = B'Length;
+     (Occ (B, Val) = Occ (A, Val) + 1) with
+   Pre => A'Length < Positive'Last;
 
    function Multiset_Minus
      (A   : T_Arr;
       B   : T_Arr;
-      Val : T) return Boolean is
-     (Count (A, Val) = Count (B, Val) - 1) with
-      Pre => A'Length = B'Length;
+     Val : T) return Boolean is
+    (Occ (B, Val) = Occ (A, Val) - 1);
 
 end Multiset_Predicates;
