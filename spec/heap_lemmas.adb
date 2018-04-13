81bc442ea66bdb3f9050e32f3b1bd8a480f515be
patouf35 <leo.creuse@gmail.com>
2018-04-13 10:18:35 +0200

    Update heap_lemmas.adb

--- spec/heap_lemmas.adb
+++ spec/heap_lemmas.adb
@@ -6,14 +6,7 @@ is
    procedure Heap_Set( H1 : Heap; H2 : Heap; P : Positive; C : Positive)
    is
    begin
-      if H1.Size = 0 then
-         return;
-      end if;
-      --pragma assert(False);
       null;
-      --pragma assert(for all K in 1 .. H1.Size => (if K /=P then H1.A(K) = H2.A(K)));
-      --pragma assert(for all K in 2 .. H2.Size => (if Heap_Parent(K) /= P then H2.A(K) <= H2.A(Heap_Parent(K))));
-      --pragma assert(H2.A(P) >= H2.A(C));
    end;
 
 
