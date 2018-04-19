09a43cfa682d350796f73752eb8b9883fc266742
Joffrey Huguet <huguet.joffrey@gmail.com>
2018-04-19 14:02:56 +0200

    make_heap: modified implementation

--- heap/make_heap_p.adb
+++ heap/make_heap_p.adb
@@ -1,37 +1,46 @@
 package body Make_Heap_P with
      Spark_Mode is
+
    function Make_Heap (A : T_Arr) return Heap is
       Result : Heap;
-      A_Save : T_Arr := Result.A with Ghost;
+      A_Save : T_Arr := Result.A with
+         Ghost;
    begin
       if A'Length > 0 then
 
          Result.A (1) := A (A'First);
-         Result.Size := 1;
+         Result.Size  := 1;
 
-         pragma Assert (Multiset_Unchanged(A(A'First .. A'First), Result.A(1 .. 1)));
+         pragma Assert
+           (Multiset_Unchanged (A (A'First .. A'First), Result.A (1 .. 1)));
 
          for J in A'First + 1 .. A'Last loop
-            Result.Size := Result.Size+1;
-            A_Save      := Result.A;
+            declare
+               Size : constant Positive := Result.Size + 1;
+            begin
+               Result.Size := Size;
+               A_Save      := Result.A;
 
-              Unchanged_Transitivity
-                (A (A'First .. J - 1),
-                 Result.A (1 .. J - A'First),
-                 A_Save (1 .. J - A'First));
+               Unchanged_Transitivity
+                 (A (A'First .. J - 1),
+                  Result.A (1 .. Size - 1),
+                  A_Save (1 .. Size - 1));
 
-            Result.A (J - A'First + 1) := A (J);
+               Result.A (Size) := A (J);
 
-            Unchanged_Transitivity (A (A'First .. J - 1), A_Save (1 .. J - A'First),Result.A (1 .. J - A'First));
-            New_Element (A (A'First .. J), Result.A (1 .. J - A'First + 1));
-            Push_Heap (Result);
+               Unchanged_Transitivity
+                 (A (A'First .. J - 1),
+                  A_Save (1 .. Size - 1),
+                  Result.A (1 .. Size - 1));
+               New_Element (A (A'First .. J), Result.A (1 .. Size));
+               Push_Heap (Result);
 
-            pragma Loop_Invariant (Result.Size = J - A'First + 1);
-            pragma Loop_Invariant (Is_Heap (Result));
-            pragma Loop_Invariant
-              (Multiset_Unchanged
-                 (A (A'First .. J),
-                  Result.A(1 .. J - A'First + 1)));
+               pragma Loop_Invariant
+                 (Result.Size = J - A'First + 1 and Result.Size = Size);
+               pragma Loop_Invariant (Is_Heap (Result));
+               pragma Loop_Invariant
+                 (Multiset_Unchanged (A (A'First .. J), Result.A (1 .. Size)));
+            end;
 
          end loop;
 
