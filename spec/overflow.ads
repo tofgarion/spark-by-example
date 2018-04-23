d864eebd8050227f2c8a80265c99bd6fe38972fc
Léo Creuse <leo.creuse@gmail.com>
2018-04-23 09:36:33 +0200

    added forgoten file

---
+++ spec/overflow.ads
@@ -0,0 +1,10 @@
+with Types; use Types;
+
+package Overflow with
+  SPARK_Mode,
+  Ghost is
+   
+   function Add_No_Overflow(X,Y : T) return Boolean is
+      (Y=0 or else (Y>0 and then T'Last - Y >= X) or else (Y<0 and then T'First - Y <= X));
+      
+end Overflow;
