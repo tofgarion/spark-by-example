0bfd6f6756abd754bf43fb625f3da019a57b10b9
Léo Creuse <leo.creuse@gmail.com>
2018-04-27 11:46:17 +0200

    inversion of adjacent difference finished

--- numeric/numeric_inv.adb
+++ numeric/numeric_inv.adb
@@ -4,7 +4,6 @@ package body Numeric_Inv with
    procedure Partial_Sum_Inv
      (A : in out T_Arr; B : in out T_Arr)
    is
-      Tmp : Constant T_Arr(A'Range) := A with Ghost;
    begin
       if A'Length = 0 then
          return;
@@ -15,4 +14,34 @@ package body Numeric_Inv with
       pragma Assert(for all J in B'First+1 .. B'Last => Minus_No_Overflow(B(J),B(J-1)));
       Adjacent_Difference(B,A);
    end Partial_Sum_Inv;
+   
+   procedure Adjacent_Difference_Inv
+     (A : in out T_Arr; B : in out T_Arr)
+   is
+      Tmp : T with Ghost;
+     -- Init : constant T_Arr (A'Range) := A with Ghost;
+   begin
+      if A'Length = 0 then
+	 return;
+      end if;
+      Tmp := A(A'First);
+      Adjacent_Difference(A,B);
+      pragma Assert(Acc_Def(B,B'First,B'First,0).OK and then Acc_Def(B,B'First,B'First,0).Value=Tmp);
+      
+      if A'Length > 1 then
+	 for J in A'First+1 .. A'Last loop
+	    
+	    pragma Assert(B(J-A'First+B'FIrst) = A(J)-Tmp);
+	    
+	    Tmp := Tmp+B(J-A'First+B'First);
+	    
+	    pragma Loop_Invariant(Acc_Def(B,B'First,J-A'First+B'First,0).OK and then Acc_Def(B,B'First,J-A'First+B'First,0).Value = Tmp);
+	    pragma Loop_Invariant(Tmp=A(J));
+	    pragma Loop_Invariant(for all K in A'First .. J => Acc_Def(B,B'First,K-A'First+B'First,0).OK and then Acc_Def(B,B'First,K-A'First+B'First,0).Value = A(K));
+	    pragma Loop_Invariant(for all K in B'First .. J-A'First+B'First=>Acc_Def(B,B'First,K,0).OK);
+	 end loop;
+      end if;
+      Partial_Sum(B,A);
+      
+   end Adjacent_Difference_Inv;
 end Numeric_Inv;
--- numeric/numeric_inv.ads
+++ numeric/numeric_inv.ads
@@ -13,5 +13,11 @@ package Numeric_Inv with
      and then (for all J in A'Range => Acc_Def(A,A'First,J,0).OK),
      Post => A = A'Old;
    
+   procedure Adjacent_Difference_Inv
+     (A : in out T_Arr; B : in out T_Arr) with
+     Pre => A'Length = B'Length
+     and then (for all J in A'First+1 .. A'Last => Minus_No_Overflow(A(J),A(J-1))),
+     Post => A = A'Old;
+   
 end Numeric_Inv;
 
