d6637062dcb1503bcb73ca836d1d807b80873a84
Christophe Garion <tofgarion@runbox.com>
2018-05-09 15:08:00 +0200

    travis: correct install of CVC4

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -5,8 +5,8 @@ wget https://github.com/Z3Prover/z3/releases/download/z3-4.6.0/z3-4.6.0-x64-ubun
 unzip z3-4.6.0-x64-ubuntu-14.04.zip
 
 # install CVC4 1.5
-wget http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-1.5-x86_64-linux-opt
-ln -sf cvc4-1.5-x86_64-linux-opt cvc4
+wget http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-1.5-x86_64-linux-opt -O cvc4
+chmod 755 cvc4
 
 # install SPARK Discovery
 wget http://mirrors.cdn.adacore.com/art/592c5299c7a447388d5c991d -O spark-discovery-gpl-2017-x86_64-linux-bin.tar.gz
