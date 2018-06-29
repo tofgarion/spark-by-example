468dc2f125d252dc7b60df2a84ca3c53c9f7c99a
fujiks <huguet.joffrey@gmail.com>
2018-06-29 15:50:05 +0200

    added chapter stats function: few errors to correct

---
+++ stats/generate_csv_chapter.py
@@ -0,0 +1,31 @@
+import sys
+
+filename = sys.argv[1]
+
+chapters = open("../stats/chapters.csv","r")
+values=[0,0,0,0,0,0]
+chapters.readline();
+for i in range(1,8):
+    algos = chapters.readline().split(",")
+    if algos[0] == filename:     
+        output = open("../stats/chapter-" + filename +".csv", "w")
+        output.write("algo,total,% proved,altergo,cvc4,z3\n")
+        algos[len(algos)-1] = algos[len(algos)-1][:-1]
+        for i in range(1,len(algos)):
+            input = open("../stats/"+algos[i]+".csv",'r')
+            input.readline()
+            for line in input :
+                tokens = line.split(",")
+                values[0]+= float(tokens[1])
+                values[2]+= float(tokens[4])
+                values[3]+= float(tokens[5])
+                values[4]+= float(tokens[6])
+                values[5]+= float(tokens[7])
+            input.close()
+            values[1]= (values[0]-values[5])/values[0]*100
+            output.write(algos[i])
+            for j in range(0,5):
+                output.write(","+str(values[j]))
+            output.write("\n")
+            values=[0,0,0,0,0,0]
+        output.close()
