e21a9a4ea3700e840f22f69a00d9a6d423b69652
fujiks <huguet.joffrey@gmail.com>
2018-07-02 16:57:42 +0200

    second script finished for chapters stats

--- stats/generate_csv_chapter_stdout.py
+++ stats/generate_csv_chapter_stdout.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import sys
 
 filename = sys.argv[1]
@@ -8,19 +11,17 @@ chapters.readline();
 for i in range(1,8):
     algos = chapters.readline().split(",")
     if algos[0] == filename:
-        output = open("../stats/chapter-" + filename +".csv", "w")
+        output = open("../stats/chapter-stdout-" + filename +".csv", "w")
         output.write("algo,total,% proved,altergo,cvc4,z3\n")
         algos[len(algos)-1] = algos[len(algos)-1][:-1]
         for j in range(1,len(algos)):
-            input = open("../stats/"+algos[j]+".csv",'r')
+            input = open("../stats/"+algos[j]+".stdout.csv",'r')
             input.readline()
-            for line in input :
-                tokens = line.split(",")
+            for k in range(1,9) :
+                tokens = input.readline().split(",")
                 values[0]+= float(tokens[1])
-                values[2]+= float(tokens[4])
-                values[3]+= float(tokens[5])
-                values[4]+= float(tokens[6])
-                values[5]+= float(tokens[7])
+                for l in range(2,6):
+                    values[l]+= float(tokens[l])
             input.close()
             values[1]= (values[0]-values[5])/values[0]*100
             output.write(algos[i])
