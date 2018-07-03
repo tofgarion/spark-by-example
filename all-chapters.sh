dabf71cac5b174357275c0ca83bae923c782865e
Joffrey Huguet <huguet.joffrey@gmail.com>
2018-07-03 16:45:46 +0200

    added script to prove all chapters

---
+++ all-chapters.sh
@@ -0,0 +1,16 @@
+cd non-mutating
+make all STATS=1
+cd ../maxmin
+make all STATS=1
+cd ../binary-search
+make all STATS=1
+cd ../mutating
+make all STATS=1
+cd ../numeric
+make all STATS=1
+cd ../heap
+make all STATS=1
+cd ../sorting
+make all STATS=1
+cd ../classic-sorting
+make all STATS=1
