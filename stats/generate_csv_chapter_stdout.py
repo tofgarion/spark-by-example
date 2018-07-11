f0b4571827e6c3377b66692b16f62ae45a1fad45
Léo Creuse <leo.creuse@gmail.com>
2018-07-11 15:03:28 +0200

    stats: added recap generation

--- stats/generate_csv_chapter_stdout.py
+++ stats/generate_csv_chapter_stdout.py
@@ -1,32 +1,29 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import sys
 
 filename = sys.argv[1]
 
 chapters = open("../stats/chapters.csv","r")
-values=[0,0,0,0,0,0]
+values=[0,0,0,0,0,0,0]
 chapters.readline();
-for i in range(1,8):
+for i in range(1,9):
     algos = chapters.readline().split(",")
     if algos[0] == filename:
         output = open("../stats/chapter-stdout-" + filename +".csv", "w")
-        output.write("algo,total,% proved,altergo,cvc4,z3\n")
+        output.write("algo,total,% proved,altergo,cvc4,z3,unproved,flow\n")
         algos[len(algos)-1] = algos[len(algos)-1][:-1]
         for j in range(1,len(algos)):
             input = open("../stats/"+algos[j]+".stdout.csv",'r')
             input.readline()
             for k in range(1,9) :
                 tokens = input.readline().split(",")
-                values[0]+= float(tokens[1])
-                for l in range(2,6):
+                values[0]+= float(tokens[1]) + float(tokens[5])
+                for l in range(2,7):
                     values[l]+= float(tokens[l])
             input.close()
             values[1]= (values[0]-values[5])/values[0]*100
             output.write(algos[j])
-            for k in range(0,5):
+            for k in range(0,7):
                 output.write(","+str(values[k]))
             output.write("\n")
-            values=[0,0,0,0,0,0]
+            values=[0,0,0,0,0,0,0]
         output.close()
---
+++ stats/generate_recap_csv.py
@@ -0,0 +1,24 @@
+chapters = ["non-mutating","maxmin","binary-search","mutating","numeric","heap","sorting","classic-sorting"]
+values=[0,0,0,0,0,0,0]
+
+output = open("../stats/recap.csv", "w")
+output.write("chapter,total,% proved,altergo,cvc4,z3,unproved,flow\n")
+for j in range(len(chapters)):
+    input = open("../stats/chapter-stdout-"+chapters[j]+".csv",'r')
+    tokens=input.readline().split(",")
+    while len(tokens)>1 :
+        tokens = input.readline().split(",")
+        print(tokens)
+        if len(tokens)==1 :
+            continue
+        values[0]+= float(tokens[1])
+        for l in range(3,8):
+            values[l-1]+= float(tokens[l])
+    input.close()
+    values[1]= (values[0]-values[5])/values[0]*100
+    output.write(chapters[j])
+    for k in range(0,7):
+        output.write(","+str(values[k]))
+    output.write("\n")
+    values=[0,0,0,0,0,0,0]
+output.close()
