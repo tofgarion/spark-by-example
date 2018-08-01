954d09dee42777188fc1568cac0a1e085ecb35a7
Christophe Garion <tofgarion@runbox.com>
2018-08-01 19:07:20 +0200

    non-mutating: first step to use new Has_Subrange function in Find_End

--- non-mutating/find_end_p.ads
+++ non-mutating/find_end_p.ads
@@ -11,15 +11,14 @@ package Find_End_P with
      (A : T_Arr;
       B : T_Arr)
       return Option with
-      Pre            => A'Last < Positive'Last and then B'First <= B'Last,
-      Contract_Cases => (B'Length = 0 => not Find_End'Result.Exists,
-       A'Length < B'Length => not Find_End'Result.Exists,
-       A'Length >= B'Length and then Has_Subrange_2 (A, B) =>
+      Pre            => A'Last < Positive'Last,
+      Contract_Cases => (
+       A'Length >= B'Length And Then B'Length /= 0 And then Has_Subrange (A, B) =>
          Find_End'Result.Exists
          and then Equal_Subrange (A, Find_End'Result.Value, B)
          and then
          (if Find_End'Result.Value < A'Last - B'Length + 1 then
-            (not Has_Subrange_In_Postfix (A, Find_End'Result.Value + 1, B))),
+            (not Has_Subrange (A (Find_End'Result.Value + 1 ..A'Last), B))),
        others => not Find_End'Result.Exists);
 
 end Find_End_P;
