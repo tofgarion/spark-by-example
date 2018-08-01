a01184930a347eb89349a53dfada5f732f59b5dc
Christophe Garion <tofgarion@runbox.com>
2018-08-01 14:52:20 +0200

    binary-search: refactor + typo Search_Lower/Upper_Bound.org

--- binary-search/search_upper_bound_p.adb
+++ binary-search/search_upper_bound_p.adb
@@ -1,6 +1,11 @@
 package body Search_Upper_Bound_P with
-     Spark_Mode is
-   function Search_Upper_Bound (A : T_Arr; Val : T) return Option is
+   Spark_Mode
+ is
+   function Search_Upper_Bound
+     (A   : T_Arr;
+      Val : T)
+      return Option
+   is
       Left   : Integer := A'First;
       Middle : Integer := A'First;
       Result : Option  := (Exists => False);
@@ -11,26 +16,23 @@ package body Search_Upper_Bound_P with
          Result := (Exists => True, Value => A'Last + 1);
          while (Left < Result.Value) loop
             Middle := Left + (Result.Value - Left) / 2;
+
             if A (Middle) <= Val then
                Left := Middle + 1;
             else
                Result.Value := Middle;
             end if;
 
+            pragma Loop_Variant (Decreases => Result.Value - Left);
             pragma Loop_Invariant (A'First <= Left);
             pragma Loop_Invariant (Left <= Result.Value);
             pragma Loop_Invariant (Result.Value <= A'Last + 1);
             pragma Loop_Invariant
-              (if
-                 Left /= A'First
-               then
+              (if Left /= A'First then
                  Upper_Bound (A (A'First .. Left - 1), Val));
             pragma Loop_Invariant
-              (if
-                 Result.Value /= A'Last + 1
-               then
+              (if Result.Value /= A'Last + 1 then
                  Strict_Lower_Bound (A (Result.Value .. A'Last), Val));
-            pragma Loop_Variant (Decreases => Result.Value - Left);
          end loop;
          return Result;
       end if;
