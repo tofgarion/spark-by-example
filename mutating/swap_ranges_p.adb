6ecd280dc187af69a7cd707edc22b535d665e7cd
Christophe Garion <tofgarion@runbox.com>
2018-08-17 13:56:57 +0200

    mutating: remove local variables in Swap_Ranges

--- mutating/swap_ranges_p.adb
+++ mutating/swap_ranges_p.adb
@@ -8,26 +8,22 @@ package body Swap_Ranges_P with
    begin
 
       for J in 0 .. A'Length - 1 loop
-         declare
-            K : constant Positive := A'First + J;
-            L : constant Positive := B'First + J;
-         begin
+         Swap (A (A'First + J), B (B'First + J));
 
-            Swap (A (K), B (L));
-
-            pragma Loop_Invariant (K = A'First + J and L = B'First + J);
-            pragma Loop_Invariant
-              (B'Loop_Entry (B'First .. L) = A (A'First .. K));
-            pragma Loop_Invariant
-              (A'Loop_Entry (A'First .. K) = B (B'First .. L));
-            pragma Loop_Invariant
-              (if L < B'Last then
-                 B'Loop_Entry (L + 1 .. B'Last) = B (L + 1 .. B'Last));
-            pragma Loop_Invariant
-              (if K < A'Last then
-                 A'Loop_Entry (K + 1 .. A'Last) = A (K + 1 .. A'Last));
-         end;
-
+         pragma Loop_Invariant
+           (B'Loop_Entry (B'First .. B'First + J) =
+            A (A'First .. A'First + J));
+         pragma Loop_Invariant
+           (A'Loop_Entry (A'First .. A'First + J) =
+            B (B'First .. B'First + J));
+         pragma Loop_Invariant
+           (if B'First + J < B'Last then
+              B'Loop_Entry (B'First + J + 1 .. B'Last) =
+              B (B'First + J + 1 .. B'Last));
+         pragma Loop_Invariant
+           (if A'First + J < A'Last then
+              A'Loop_Entry (A'First + J + 1 .. A'Last) =
+              A (A'First + J + 1 .. A'Last));
       end loop;
    end Swap_Ranges;
 end Swap_Ranges_P;
