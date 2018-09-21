76beef7c871871e41ccef49b46de6c35f04d7e9e
Christophe Garion <tofgarion@runbox.com>
2018-09-21 13:18:47 +0200

    scripts: Python script to remove pragmas etc. for readibility

---
+++ scripts/remove_assertions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3 -O
+
+"""Remove pragma directives in Ada source code. First argument is the
+file to be treated. The created filename will have \"_wo_pragmas\"
+added. You can give more arguments to eliminate lines containing some
+strings, for instance to remove call to lemmas.
+
+Beware, this is a cheap and dirty script, it may not work with your
+code!
+
+"""
+
+import os
+import sys
+
+multi_line_pragma = False
+pragma_end = False
+excluding_list = [ "pragma" ]
+
+if len(sys.argv) >= 2:
+    excluding_list = excluding_list + sys.argv[2:]
+
+with open(sys.argv[1]) as source_file, open(os.path.splitext(sys.argv[1])[0] + "_wo_pragmas.adb", 'w') as dest_file:
+    for row in source_file:
+        if pragma_end:
+            pragma_end = False
+            if row == "\n":
+                continue
+        if multi_line_pragma and ";" not in row:
+            continue
+        else:
+            if any(s in row for s in excluding_list):
+                if ";" not in row:
+                    multi_line_pragma = True
+                else:
+                    pragma_end = True
+                continue
+            elif multi_line_pragma and ";" in row:
+                multi_line_pragma = False
+                pragma_end = True
+                continue
+            else:
+                dest_file.write(row)
